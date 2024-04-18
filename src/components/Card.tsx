18dd2b25417641d0a148b27f0393d4b59ef178cf
OstapivIvan <ostapivivan05@gmail.com>
2024-04-18 23:08:40 +0300

    Add Card component for displaying stadium information and a remove button

---
+++ src/components/Card.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Button from "./Button";
+
+export interface CardProps {
+    stadiumName: string;
+    city: string;
+    capacity: string;
+    fieldType: string;
+    id: string;
+    onClick: (id: string) => void;
+}
+
+const Card: React.FC<CardProps> = ({
+    stadiumName,
+    city,
+    capacity,
+    fieldType,
+    id,
+    onClick,
+}) => {
+    const handleRemove = () => {
+        onClick(id);
+    };
+
+    return (
+        <div className="card-container" id={id}>
+            <div className="card-item">Stadium - {stadiumName}</div>
+            <div className="card-item">City - {city}</div>
+            <div className="card-item">Capacity - {capacity}</div>
+            <div className="card-item">Field - {fieldType}</div>
+            <div className="form-buttons card-button">
+                <Button type="button" onClick={handleRemove}>
+                    Remove
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
